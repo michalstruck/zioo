48cee345d80c72afe1ef56967bc61f5afb8646fe
Michal Struck <michal.o.struck@gmail.com>
2026-04-23 01:46:45 +0200

    center header

--- zioo/app/checkout/layout.tsx
+++ zioo/app/checkout/layout.tsx
@@ -28,20 +28,23 @@ export default function CheckoutLayout({
         href="https://geowidget.inpost.pl/inpost-geowidget.css"
       />
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
-        <div className="container flex h-16 items-center justify-center">
-          <Link
-            href="/store"
-            className="flex items-center flex-1 justify-start ml-4 gap-2 text-md font-medium transition-colors hover:text-primary"
-          >
-            <ArrowLeft className="size-6" />
-            Wróć do sklepu
-          </Link>
+        <div className="flex h-16 w-full items-center px-4">
+          <div className="flex-1">
+            <Link
+              href="/store"
+              className="inline-flex items-center gap-2 text-md font-medium transition-colors hover:text-primary"
+            >
+              <ArrowLeft className="size-6" />
+              Wróć do sklepu
+            </Link>
+          </div>
           <Link
             href="/"
-            className="text-2xl flex-1 font-heading font-medium tracking-tight sm:text-3xl md:text-4xl hover:text-primary transition-organic"
+            className="text-4xl font-heading font-medium tracking-tight hover:text-primary transition-organic"
           >
             zioo
           </Link>
+          <div className="flex-1" />
         </div>
       </header>
       <main className="flex-1 flex flex-col pt-8 md:pt-12 px-5">
